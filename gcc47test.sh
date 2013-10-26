b8431f42f00f59ea615dc0329b4d06547c628f73
Jeremy Meile <jeremymeile@brainmac.home>
2013-10-26 13:54:34 +0200

    fix

--- gcc47test.sh
+++ gcc47test.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
+if f=$(xcode-select --print-path)
+then
+echo
+		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk'
+		then
+			export DEPLOYMENT_TARGET=10.6
+			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
+			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
+			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
+		fi
+		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk'
+		then
+			export DEPLOYMENT_TARGET=10.7
+			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
+			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
+			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
+		fi
+		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk'
+		then
+			export DEPLOYMENT_TARGET=10.8
+			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+		fi
+		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk'
+		then
+			export DEPLOYMENT_TARGET=10.9
+			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
+		fi
+else
+	echo -e 'Install and initialize XCode first!'
+	exit 1
+fi
 INSTALL_PATH="/Applications/gcc47"
 cd ~/Downloads
 rm -d -f -r gmp-5.1.1
