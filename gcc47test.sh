a4540e996b6df28da659ad5b23c4c0556dc021c9
Jeremy Meile <jeremymeile@brainmac.home>
2013-11-02 19:38:23 +0100

    lol

--- gcc47test.sh
+++ gcc47test.sh
@@ -1,54 +1,23 @@
-#!/bin/bash
-if f=$(xcode-select --print-path)
-then
-echo
-		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk'
-		then
-			export DEPLOYMENT_TARGET=10.6
-			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
-			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
-			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
-		fi
-		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk'
-		then
-			export DEPLOYMENT_TARGET=10.7
-			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
-			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
-			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk"
-		fi
-		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk'
-		then
-			export DEPLOYMENT_TARGET=10.8
-			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
-			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
-			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
-			echo $f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
-		fi
-		if test -d $f'/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk'
-		then
-			export DEPLOYMENT_TARGET=10.9
-			export OSX_SDK="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
-			export OSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
-			export MACOSX_SYSROOT="$f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"
-			echo $f/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
-		fi
-else
-	echo -e 'Install and initialize XCode first!'
-	exit 1
-fi
-echo 
+#!/bin/bash 
 INSTALL_PATH="/Applications/gcc47"
 cd ~/Downloads
-rm -d -f -r gmp-5.1.1
-rm -d -f -r gmp-5.1.1.tar.bz2
-curl -O ftp://ftp.gmplib.org/pub/gmp/gmp-5.1.1.tar.bz2
-tar -jxvf gmp-5.1.1.tar.bz2
-cd gmp-5.1.1
+rm -d -f -r gmp-5.1.3
+rm -d -f -r gmp-5.1.3.tar.bz2
+curl -O ftp://ftp.gmplib.org/pub/gmp/gmp-5.1.3.tar.bz2
+tar -jxvf gmp-5.1.3.tar.bz2
+cd gmp-5.1.3
 mkdir build
 cd build
-../configure --prefix=$INSTALL_PATH --enable-cxx
+export DEPLOYMENT_TARGET=10.8
+export OSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export MACOSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+#export CC="/gcc/bin/gcc"
+#export CXX="/gcc/bin/c++"
+../configure --prefix=/Applications/gcc47 --enable-cxx
 make -j $(sysctl -n hw.ncpu)
 make install
+
 cd ~/Downloads
 rm -d -f -r mpfr-3.1.2
 rm -d -f -r mpfr-3.1.2.tar.gz
@@ -57,9 +26,14 @@ tar -jxvf mpfr-3.1.2.tar.gz
 cd mpfr-3.1.2
 mkdir build
 cd build
-../configure --prefix=$INSTALL_PATH --with-gmp=$INSTALL_PATH
+export DEPLOYMENT_TARGET=10.8
+export OSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export MACOSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+../configure --prefix=/Applications/gcc47 --with-gmp=/Applications/gcc47
 make -j $(sysctl -n hw.ncpu)
 make install
+
 cd ~/Downloads
 rm -d -f -r mpc-1.0.1
 rm -d -f -r mpc-1.0.1.tar.gz
@@ -68,17 +42,27 @@ tar -jxvf mpc-1.0.1.tar.gz
 cd mpc-1.0.1
 mkdir build
 cd build
-../configure --prefix=$INSTALL_PATH --with-gmp=$INSTALL_PATH --with-mpfr=$INSTALL_PATH
+export DEPLOYMENT_TARGET=10.8
+export OSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export MACOSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+../configure --prefix=/Applications/gcc47 --with-gmp=/Applications/gcc47 --with-mpfr=/Applications/gcc47
 make -j $(sysctl -n hw.ncpu)
 make install
+
 cd ~/Downloads
 rm -d -f -r gcc-4.7.3
 rm -d -f -r gcc-4.7.3.tar.bz2
-curl -O http://gcc-uk.internet.bs/releases/gcc-4.7.3/gcc-4.7.3.tar.bz2 > /dev/null 2>/tmp/Acetool_errorMSG
+curl -O http://gcc-uk.internet.bs/releases/gcc-4.7.3/gcc-4.7.3.tar.bz2
 tar -jxvf gcc-4.7.3.tar.bz2
 cd gcc-4.7.3
+rm -d -f -r build
 mkdir build
 cd build
-../configure --enable-checking=release --program-suffix=-4.7 --enable-languages=c,c++,fortran --prefix=$INSTALL_PATH --with-gmp=$INSTALL_PATH --with-mpfr=$INSTALL_PATH --with-mpc=$INSTALL_PATH > /dev/null 2>/tmp/Acetool_errorMSG
+export DEPLOYMENT_TARGET=10.8
+export OSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+export MACOSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk"
+../configure --enable-checking=release --program-suffix=-4.7 --enable-checking=release --prefix=/Applications/gcc47 --with-gmp=/Applications/gcc47 --with-mpfr=/Applications/gcc47 --with-mpc=/Applications/gcc47
 make -j $(sysctl -n hw.ncpu)
-make install > /dev/null
+make install
